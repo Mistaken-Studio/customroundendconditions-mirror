f908e4a6d5c1fd9c2afcc59084896656e8ee65f1
Tajemniczy <SFBreakTF2@gmail.com>
2022-01-08 18:41:26 +0100

    dev commit

--- CustomRoundEndConditions/Config.cs
+++ CustomRoundEndConditions/Config.cs
@@ -4,8 +4,8 @@
 // </copyright>
 // -----------------------------------------------------------------------
 
-using Mistaken.Updater.Config;
 using System.ComponentModel;
+using Mistaken.Updater.Config;
 
 namespace Mistaken.CustomRoundEndConditions
 {
@@ -24,5 +24,29 @@ namespace Mistaken.CustomRoundEndConditions
         /// <inheritdoc/>
         [Description("Auto Update Settings")]
         public System.Collections.Generic.Dictionary<string, string> AutoUpdateConfig { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether should the round end when SCPs and CI are alive.
+        /// </summary>
+        [Description("If true the round will end when SCPs and CI are alive.")]
+        public bool ScpCiWin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the percentage of Class D to escape for win.
+        /// </summary>
+        [Description("Class D Escape percentage to win")]
+        public decimal ClassDEscape { get; set; } = 50;
+
+        /// <summary>
+        /// Gets or sets the percentage of Scientists to escape for MTF to win.
+        /// </summary>
+        [Description("Scientists Escape percentage for MTF to win")]
+        public decimal ScientistsEscape { get; set; } = 50;
+
+        /// <summary>
+        /// Gets or sets the percentage of Scientists to escape for MTF to win when only MTF is alive.
+        /// </summary>
+        [Description("Scientists Escape percentage for MTF to win when only MTF is alive.")]
+        public decimal ScientistsEscapeOnlyMTFAlive { get; set; } = 50;
     }
 }
---
+++ CustomRoundEndConditions/CustomRoundEndConditionsHandler.cs
@@ -0,0 +1,75 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CustomRoundEndConditionsHandler.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Linq;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using Mistaken.API.Diagnostics;
+
+namespace Mistaken.CustomRoundEndConditions
+{
+    internal class CustomRoundEndConditionsHandler : Module
+    {
+        public CustomRoundEndConditionsHandler(PluginHandler plugin)
+            : base(plugin)
+        {
+        }
+
+        public override string Name => "CustomRoundEndConditionsHandler";
+
+        public override void OnEnable()
+        {
+            Exiled.Events.Handlers.Server.EndingRound += this.Server_EndingRound;
+        }
+
+        public override void OnDisable()
+        {
+            Exiled.Events.Handlers.Server.EndingRound -= this.Server_EndingRound;
+        }
+
+        private void Server_EndingRound(EndingRoundEventArgs ev)
+        {
+            int ciAlive = Player.List.Where(x => x.IsCHI).Count();
+            int scpAlive = Player.List.Where(x => x.IsScp).Count();
+            int mtfAlive = Player.List.Where(x => x.IsNTF).Count();
+            int notMTFAlive = Player.List.Where(x => !x.IsNTF).Count();
+            int notSCPAlive = Player.List.Where(x => !x.IsScp).Count();
+            if (!PluginHandler.Instance.Config.ScpCiWin && ciAlive != 0 && scpAlive != 0)
+            {
+                ev.IsAllowed = false;
+            }
+            else if (PluginHandler.Instance.Config.ClassDEscape <= (RoundSummary.EscapedClassD / RoundSummary.singleton.CountRole(RoleType.ClassD) * 100))
+            {
+                this.Log.Debug($"Class D won. {RoundSummary.EscapedClassD / RoundSummary.singleton.CountRole(RoleType.ClassD) * 100}% Escaped. {PluginHandler.Instance.Config.ClassDEscape}% Required.", PluginHandler.Instance.Config.VerbouseOutput);
+                ev.LeadingTeam = LeadingTeam.ChaosInsurgency;
+            }
+            else if (PluginHandler.Instance.Config.ScientistsEscape <= (RoundSummary.EscapedScientists / RoundSummary.singleton.CountRole(RoleType.Scientist) * 100) || (PluginHandler.Instance.Config.ScientistsEscapeOnlyMTFAlive >= (RoundSummary.EscapedScientists / RoundSummary.singleton.CountRole(RoleType.Scientist)) && mtfAlive != 0 && notMTFAlive == 0))
+            {
+                this.Log.Debug($"MTF won. {RoundSummary.EscapedScientists / RoundSummary.singleton.CountRole(RoleType.Scientist) * 100}% Scientists Escaped. {PluginHandler.Instance.Config.ScientistsEscape}% Required.\n{mtfAlive} MTF Alive\n{notMTFAlive} Others Alive.", PluginHandler.Instance.Config.VerbouseOutput);
+                ev.LeadingTeam = LeadingTeam.FacilityForces;
+            }
+            else if (PluginHandler.Instance.Config.ScientistsEscape <= (RoundSummary.EscapedScientists / RoundSummary.singleton.CountRole(RoleType.Scientist) * 100) || (PluginHandler.Instance.Config.ScientistsEscapeOnlyMTFAlive >= (RoundSummary.EscapedScientists / RoundSummary.singleton.CountRole(RoleType.Scientist)) && mtfAlive != 0 && notMTFAlive == 0))
+            {
+                this.Log.Debug($"MTF won. {RoundSummary.EscapedScientists / RoundSummary.singleton.CountRole(RoleType.Scientist) * 100}% Scientists Escaped. {PluginHandler.Instance.Config.ScientistsEscape}% Required.\n{mtfAlive} MTF Alive\n{notMTFAlive} Others Alive.", PluginHandler.Instance.Config.VerbouseOutput);
+                ev.LeadingTeam = LeadingTeam.FacilityForces;
+            }
+            else if (scpAlive != 0 && notSCPAlive == 0)
+            {
+                this.Log.Debug($"SCP won. {scpAlive} SCPs Left. {notSCPAlive} Humans Left.", PluginHandler.Instance.Config.VerbouseOutput);
+                ev.LeadingTeam = LeadingTeam.FacilityForces;
+            }
+            else
+            {
+                this.Log.Debug($"No one won.\n{RoundSummary.EscapedClassD / RoundSummary.singleton.CountRole(RoleType.ClassD) * 100}% Class D Escaped. {PluginHandler.Instance.Config.ClassDEscape}% Required.\n{RoundSummary.EscapedScientists / RoundSummary.singleton.CountRole(RoleType.Scientist) * 100}% Scientists Escaped. {PluginHandler.Instance.Config.ScientistsEscape}% Required.\n{mtfAlive} MTF Alive\n{notMTFAlive} Others Alive.", PluginHandler.Instance.Config.VerbouseOutput);
+                ev.LeadingTeam = LeadingTeam.Draw;
+            }
+
+            ev.IsRoundEnded = true;
+            ev.IsAllowed = true;
+        }
+    }
+}
